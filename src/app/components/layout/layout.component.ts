f5f1b7d2c57398afdba8c3c38d1f36ec581063e2
Zahed75 <zahedhasan.cs@gmail.com>
2025-03-16 01:27:01 +0600

    Layout updated

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [
+
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+
+  isOpenProfile=false;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+
+  isActive(route: string): boolean {
+
+    return false;
+  }
+
+  toggleDemoMode(event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+    console.log('Demo Mode:', checkbox.checked);
+
+  }
+
 
 }

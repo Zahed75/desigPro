7e378070bd985287b7c41671fcb154b588118cbe
Zahed75 <zahedhasan.cs@gmail.com>
2025-03-15 22:57:04 +0600

    Auth Design DOne

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,24 +1,26 @@
 import { NgClass, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
   imports: [
-  
-    
+    RouterLink
+
+
   ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent implements OnInit {
-  
+
   constructor(){
 
   }
 
   ngOnInit(): void {
-    
+
   }
 
 
